18dca79775cd060206f6de56a05b7c8349505b04
lucianokatze <ventaonlinetotalsport@gmail.com>
2024-04-23 09:56:11 -0300

    Update WhatsappPage component with new message state

--- app/whatsapp/page.tsx
+++ app/whatsapp/page.tsx
@@ -3,15 +3,16 @@ import Link from "next/link"
 import { useState } from "react";
 import { useQRCode } from "next-qrcode"
 import { closeClient, initClient } from "@/app/api/botApi"
-import { EVENT_TYPE, IState, STATUS } from "@/app/types/types"
+import { EVENT_TYPE, IState, STATUS, WHATSAPP_EVENT } from "@/app/types/types"
 import { machine } from "@/app/whatsapp/state/machine"
 import socket from "@/app/sockets/socket"
 
 export default function WhatsappPage() {
     const { Canvas } = useQRCode()
     const [state, setState] = useState<IState>({
-        status: STATUS.LOADING,
-        qr: ""
+        status: STATUS.IDLE,
+        qr: "",
+        message: ["*Venta artículo publicado Online* Usuario: *Acosta Sebastian*, Sucursal: *11 - Ansilta Exclusivo* Venta: *FcB 0073-00004113* emitida el *22/04/2024 19:28* hs. Id: 273648 SKU: *ANS123117210$L* Item: *CAMPERA INKEN 2 - WINDSTOPPER ALLIED 800* Talle: *L* Cantidad: *1* Importe: $501.500,00"]
     })
 
     socket.on(EVENT_TYPE.BOT, (event) => setState(machine(state, event)))
@@ -30,6 +31,12 @@ export default function WhatsappPage() {
             console.log(error)
         }
     }
+
+    // socket.on(EVENT_TYPE.BOT, (event) => {
+    //     if(event.type === WHATSAPP_EVENT.MESSAGE){
+    //         console.log(event.payload.message)
+    //     }
+    // })
 
     const renderControl = () => {
         switch (state.status) {
@@ -51,6 +58,7 @@ export default function WhatsappPage() {
             {renderControl()}
             {state.status === STATUS.CLIENT_READY && <p>B0T conectado</p>}
             {state.qr !== "" && <Canvas text={state.qr} />}
+            {state.message.map((msg, index) => <p key={index}>{msg}</p>)}
         </div>
     )
 }
